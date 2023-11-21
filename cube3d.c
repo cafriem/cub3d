893344f553e080c7de4c373c550b468d0b1d9888
Christian Afriem <cafriem@lab1r4s1.42abudhabi.ae>
2023-11-21 18:39:28 +0400

    idk

--- cube3d.c
+++ cube3d.c
@@ -6,7 +6,7 @@
 /*   By: cafriem <cafriem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 16:42:29 by cafriem           #+#    #+#             */
-/*   Updated: 2023/11/17 16:31:09 by cafriem          ###   ########.fr       */
+/*   Updated: 2023/11/21 16:03:40 by cafriem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,13 +50,18 @@ void	texture_parse(t_data *img)
 	}
 }
 
+int	create_trgb(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
+
 void	struck_check(t_data *img)
 {
 	printf("NO texture = %s\n", img->t_n);
 	printf("SO texture = %s\n", img->t_s);
 	printf("EA texture = %s\n", img->t_e);
 	printf("WE texture = %s\n", img->t_w);
-	printf("t = %d\n", create_trgb(1, 255, 255, 255));
+	printf("t = %d\n", create_trgb(1, 220, 100, 0));
 	// printf("F = %s\n", img->f);
 	// printf("C = %s\n", img->c);
 }
@@ -84,17 +89,17 @@ int	main(int argc, char *argv[])
 		ft_putstr_fd("WRONG AMOUNT OF ARGUEMENTS", 2);
 		exit(1);
 	}
-	img.mlx = mlx_init();
-	init(&img);
-	img.mlx_win = mlx_new_window(img.mlx, 1080, 720, argv[1]);
-	img.img = mlx_new_image(img.mlx, 1080, 720);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
-		&img.line_length, &img.endian);
+	// img.mlx = mlx_init();
+	// init(&img);
+	// img.mlx_win = mlx_new_window(img.mlx, 1080, 720, argv[1]);
+	// img.img = mlx_new_image(img.mlx, 1080, 720);
+	// img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
+	// 	&img.line_length, &img.endian);
 	openmap(&img, argv);
-	mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, 0, 0);
-	legends1(&img);
-	mlx_hook(img.mlx_win, 2, 1L << 2, key_press1, &img);
-	mlx_loop(img.mlx);
+	// mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, 0, 0);
+	// legends1(&img);
+	// mlx_hook(img.mlx_win, 2, 1L << 2, key_press1, &img);
+	// mlx_loop(img.mlx);
 }
 
 // A* search algorithm
