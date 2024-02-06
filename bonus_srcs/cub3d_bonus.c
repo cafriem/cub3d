4d42859260aebefa712a9b2b88002a4bea4a1b4e
John Adithya <jadithya@lab3r2s10.42abudhabi.ae>
2024-02-06 16:34:43 +0400

    fixed sprint error

--- bonus_srcs/cub3d_bonus.c
+++ bonus_srcs/cub3d_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jadithya <jadithya@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 18:31:36 by jadithya          #+#    #+#             */
-/*   Updated: 2024/01/25 17:12:26 by jadithya         ###   ########.fr       */
+/*   Updated: 2024/02/06 16:32:22 by jadithya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -129,9 +129,9 @@ int	main(int argc, char *argv[])
 	create_map(&cube);
 	print_filemap(&cube);
 	mlx_hook(cube.mlx_window, 17, 0, close_x, &cube);
-	mlx_hook(cube.mlx_window, 2, (1L << 0), keydown, &cube);
+	mlx_hook(cube.mlx_window, 2, 1L << 0, keydown, &cube);
 	mlx_hook(cube.mlx_window, 3, (1L << 1), keyup, &cube);
-	mlx_hook(cube.mlx_window, 6,(1L << 6), mouse, &cube);
+	//mlx_hook(cube.mlx_window, 6, (1L << 6), mouse, &cube);
 	mlx_loop_hook(cube.mlx, move, &cube);
 	mlx_loop(cube.mlx);
 }
